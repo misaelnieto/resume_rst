ba6848aae3a1ca487be2727cd6cebb99d52e19c8
tzicatl <nnieto@noenieto.com>
2012-07-23 22:15:21 -0500

    Simple build script and PDF styles :)

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Made by Noe Nieto. No copyright. Do what you want with your copy of this 
+#file.
+#With this I build my documents.
+#You will need to install rst2pdf either with yum, apt, pip, easy_install or 
+#whatever else ;)
+rst2pdf -s styles.json --font-path fonts/ electronics/en/README.txt
+
